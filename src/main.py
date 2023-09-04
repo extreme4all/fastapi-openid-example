192c71bf9b8c6144914e27ae63d911c98c5ac8bb
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-09-04 23:18:59 +0200

    added local validation

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
-import time
-
 import httpx  # httpx > aiohttp because httpx is used in depending lib
+import jwt
+from jwt.jwks_client import PyJWKClient
 from authlib.integrations.starlette_client import OAuth, StarletteOAuth2App
-from authlib.jose import JoseError, jwt
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.requests import Request
-import asyncio
 
 
 class Settings(BaseSettings):
@@ -38,8 +36,46 @@ app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="some-random-string-qsdfqsdf")
 
 
+async def fetch_jwt_key(key_url, kid):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(key_url)
+        keys = response.json()
+
+    # Find the key with the matching 'kid' (Key ID)
+    for key in keys.get("keys", []):
+        if key.get("kid") == kid:
+            return key
+
+    return None
+
+
+async def validate_token(token, audience, issuer, jwks_uri):
+    print("local validation")
+    try:
+        jwks_client = PyJWKClient(jwks_uri)
+        signing_key = jwks_client.get_signing_key_from_jwt(token)
+
+        # Validate the token using the fetched key
+        decoded_token = jwt.decode(
+            token,
+            signing_key.key,
+            algorithms=["RS256"],  # Specify the algorithm used for signing
+            audience=audience,
+            issuer=issuer,
+        )
+
+        return decoded_token
+    except jwt.ExpiredSignatureError:
+        return {"error": "Token has expired"}
+    except jwt.InvalidTokenError:
+        return {"error": "Invalid token signature"}
+    except Exception as e:
+        return {"error": str(e)}
+
+
 # the simplest way
 async def token_introspect(access_token, introspection_url):
+    print("introspect")
     async with httpx.AsyncClient() as client:
         response = await client.post(
             introspection_url,
@@ -68,22 +104,64 @@ async def login_via_google(request: Request):
 
 @app.get("/callback")
 async def callback(request: Request):
+    # this should also do all the checking ???
     token: dict = await oauth.authorize_access_token(request)
-    access_token = token.get("access_token")
-    id_token = token.get("id_token")
-    userinfo = token.get("userinfo")
+    access_token: str = token.get("access_token")
+    id_token: str = token.get("id_token")
+    userinfo: dict = token.get("userinfo")
 
-    # Validate the access token
-    access_token_info: dict = await token_introspect(
+    # Validate the tokens
+    # introspect
+    # access token
+    resp: dict = await token_introspect(
         access_token, settings.metadata.get("introspection_endpoint")
     )
-    if not access_token_info.get("active"):
+    if not resp.get("active"):
         return {
             "error": "Access token is not valid",
-            "access_token_info": access_token_info,
+            "access_token_info": resp,
             "token": token,
         }
 
+    # id token
+    resp: dict = await token_introspect(
+        id_token, settings.metadata.get("introspection_endpoint")
+    )
+    if not resp.get("active"):
+        return {
+            "error": "Access token is not valid",
+            "access_token_info": resp,
+            "token": token,
+        }
+
+    # local token validation
+    # access token
+    resp = await validate_token(
+        access_token,
+        jwks_uri=settings.metadata.get("jwks_uri"),
+        audience="api://default",
+        issuer=settings.metadata.get("issuer"),
+    )
+    if resp.get("error"):
+        return {
+            "error": "Access token is not valid",
+            "access_token_info": resp.get("error"),
+            "token": token,
+        }
+
+    # id token
+    resp = await validate_token(
+        id_token,
+        jwks_uri=settings.metadata.get("jwks_uri"),
+        audience=settings.client_id,
+        issuer=settings.metadata.get("issuer"),
+    )
+    if resp.get("error"):
+        return {
+            "error": "Access token is not valid",
+            "access_token_info": resp.get("error"),
+            "token": token,
+        }
     return dict(token)
 
 
